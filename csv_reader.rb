713d2d83f47a28b34dbec13797245a3a90c1bbd0
David Wickes <dave.wickes@gmail.com>
2014-06-11 17:23:43 +0100

    found the CSV class...

--- csv_reader.rb
+++ csv_reader.rb
@@ -1,32 +1,18 @@
 #!/usr/bin/ruby
 require 'fileutils'
+require 'CSV'
 
 if ARGV.empty?
   print "Name of csv file to operate on: "
   filename = gets.chomp
+  print "\n Does the file have headers (y/n): "
+  headers = gets.chomp
 else
   filename = ARGV.shift
+  ARGV.empty? ? headers = "n" : headers = ARGV.shift
 end
 
-csv_array = File.readlines(filename)
-header = csv_array[0].split(",")
-header.last.chomp!
-p header
-header.map!.with_index {|heading, position| [heading, position]}
-header = Hash[*header.flatten]
-p header
-
-header_order = [3,2,1]
-
-for line in csv_array[1..csv_array.length]
-  p line
-  p line.split(",")
-  line_array= line.split(",")
-  line_array.last.chomp!
-  p line_array
-  file_name = ""
-  for title in header_order
-    file_name = file_name + line_array[title]
-  end
-  p file_name
-end
+p headers
+csv = CSV.read(filename, headers: true)
+p csv.headers
+p csv
