8e8e15e5b11d0322086926c4dd58aa87744cc7d9
David Wickes <dave.wickes@gmail.com>
2014-06-11 23:48:29 +0100

    created menu

--- csv_reader.rb
+++ csv_reader.rb
@@ -1,18 +1,19 @@
 #!/usr/bin/ruby
 require 'fileutils'
-require 'CSV'
+require 'csv'
 
 if ARGV.empty?
   print "Name of csv file to operate on: "
   filename = gets.chomp
-  print "\n Does the file have headers (y/n): "
-  headers = gets.chomp
 else
   filename = ARGV.shift
-  ARGV.empty? ? headers = "n" : headers = ARGV.shift
 end
 
-p headers
-csv = CSV.read(filename, headers: true)
-p csv.headers
-p csv
+csv_array = CSV.read(filename, headers: true)
+p csv_array.headers
+p csv_array
+puts "\n------\nHeaders\n------\n"
+puts "These are the headers of the csv file:\n"
+csv_array.headers.each.with_index do
+    |header, index| puts "#{index}: #{header}"
+end
