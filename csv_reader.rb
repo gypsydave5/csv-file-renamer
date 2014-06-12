8b0344694243c76350364d218bba1d1ef2d26e51
David Wickes <dave.wickes@gmail.com>
2014-06-12 14:16:54 +0100

    interactive menu created

--- csv_reader.rb
+++ csv_reader.rb
@@ -17,3 +17,28 @@ puts "These are the headers of the csv file:\n"
 csv_array.headers.each.with_index do
     |header, index| puts "#{index}: #{header}"
 end
+
+puts "\nPlease enter an index number to add the header to the new file name."
+input = "1"
+file_name_construct = []
+while (input != "d") && (input != "")
+  print "\nFile name is: "
+  file_name_construct.each {|index| print "<" + csv_array.headers[index] + ">_"}
+  puts "\nAvailable headers:"
+  csv_array.headers.each.with_index do
+    |header, index| puts "#{index}: #{header}"
+  end
+  puts "Next header ('d' when DONE, 'o' to start OVER): "
+  input = gets.chomp
+  case input
+    when "o"
+      file_name_construct = []
+    else
+      file_name_construct << input.to_i
+  end
+end
+
+file_name_construct.pop
+ print "\nFile name is: "
+  file_name_construct.each {|index| print "<" + csv_array.headers[index] + ">_"}
+
