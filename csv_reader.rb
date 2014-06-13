d3bf01f4f6856eb48586afe8f2857396ff6f9d15
David Wickes <dave.wickes@gmail.com>
2014-06-13 14:24:26 +0100

    Create file renaming hash and test files

--- csv_reader.rb
+++ csv_reader.rb
@@ -12,6 +12,15 @@ def generate_file_name(file_format_array, csv_name, row_number, seperator="_", b
   file_name
 end
 
+def check_format_array(file_format_array, csv_name)
+  file_format_array.each do |index|
+    if index >= csv_name[0].length
+      return false
+    end
+  end
+  return true
+end
+
 if ARGV.empty?
   print "Name of csv file to operate on: "
   filename = gets.chomp
@@ -20,32 +29,25 @@ else
 end
 
 csv_array = CSV.read(filename)
-p csv_array[0]
-p csv_array
-puts "\n------\nHeaders\n------\n"
-puts "These are the headers of the csv file:\n"
-csv_array[0].each.with_index do
-    |header, index| puts "#{index}: #{header}"
-end
 
-puts "\nPlease enter an index number to add the header to the new file name."
 input = "1"
 file_name_construct = []
 while input != ""
-    if !(file_name_construct.empty?) && (file_name_construct.last >= csv_array[0].length)
-        puts "\nInvalid index number... reverting"
+  if (!check_format_array(file_name_construct, csv_array)) || (input.to_i.to_s != input)
+    puts "\nInvalid input ... reverting..."
     file_name_construct.pop
-    end
-  if file_name_construct.empty?  puts "No file format currently created"
+  end
+  if file_name_construct.empty?
+    puts "No file format currently created"
   else
   print "\nFile name format is: "
   print generate_file_name(file_name_construct, csv_array, 0, "_", ["<",">"])
   end
-  puts "\nAvailable headers:"
+  puts "\nAvailable headers:\n"
   csv_array[0].each.with_index do
     |header, index| puts "#{index}: #{header}"
   end
-  print "Next header (hit RETURN when DONE, 'o' to start OVER, 'd' to delete last header): "
+  print "\nPlease enter index of next header in file name\n(hit RETURN when DONE, 'o' to start OVER, 'd' to delete last header): "
   input = gets.chomp
   case input
     when "o"
@@ -58,7 +60,35 @@ while input != ""
 end
 
 file_name_construct.pop
+if file_name_construct.empty?
+  puts "No format created for renaming operation. Exiting..."
+  exit
+end
 print "\nFile name format is: "
 puts generate_file_name(file_name_construct, csv_array, 0, "_", ["<",">"])
+
+print "\nPlease enter file path of file(s) to be renamed (or ENTER for current): "
+filepath = gets.chomp
+if filepath == ""
+  filepath = "."
+end
+
+print "\nWhich files would you like to rename?\nPlease enter search string (or ENTER for all): "
+Dir.chdir(filepath)
+search_string = gets.chomp
+
+if search_string == ""
+  search_string = "*.*"
+end
+
+file_array = Dir[search_string]
+p file_array.sort! {|x, y| x.to_i <=> y.to_i }
+
+file_rename_map = {}
+file_array.each.with_index do |file, index|
+  file_rename_map[file] = generate_file_name(file_name_construct, csv_array, index + 1)
+end
+
+p file_rename_map
 
 
