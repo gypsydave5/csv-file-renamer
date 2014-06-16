3ab74e505e1d3f5e06dc6f3b58ab44b0b6e95bdd
David Wickes <dave.wickes@gmail.com>
2014-06-16 17:48:28 +0100

    File renaming implemented - badly. Attempted to remove whitespace breaks program. Needs work...

--- csv_reader.rb
+++ csv_reader.rb
@@ -2,13 +2,15 @@
 require 'fileutils'
 require 'csv'
 
-def generate_file_name(file_format_array, csv_name, row_number, seperator="_", braces=["",""])
+def generate_file_name(file_format_array, csv_name, row_number, seperator="_", braces=["",""], extension="")
   file_name = ""
   file_name << braces[0] + csv_name[row_number][file_format_array.first] + braces[1]
   file_format_array.drop(1).each do |index|
     file_name << seperator
     file_name << braces[0] + csv_name[row_number][index] + braces[1]
   end
+  file_name << extension
+  file_name = reformat_filename(file_name)
   file_name
 end
 
@@ -19,6 +21,10 @@ def check_format_array(file_format_array, csv_name)
     end
   end
   return true
+end
+
+def reformat_filename (filename)
+  filename.gsub! (/\s/) , "-"
 end
 
 if ARGV.empty?
@@ -88,11 +94,27 @@ end
 file_array = Dir[search_string]
 p file_array.sort! {|x, y| x.to_i <=> y.to_i }
 
-file_rename_map = {}
-file_array.each.with_index do |file, index|
-  file_rename_map[file] = generate_file_name(file_name_construct, csv_array, index + 1)
+puts "Extension for new files (return to maintain current): "
+file_extension = gets.chomp
+
+if file_extension != ""
+  file_rename_map = {}
+  file_array.each.with_index do |file, index|
+    file_rename_map[file] = generate_file_name(file_name_construct, csv_array, index + 1) + file_extension
+  end
+  else
+  file_rename_map = {}
+  file_array.each.with_index do |file, index|
+    extension = file.match (/\..*/)
+    file_rename_map[file] = generate_file_name(file_name_construct, csv_array, index + 1) + extension[0]
+  end
 end
 
 p file_rename_map
 
+file_rename_map.each do |old_name, new_name|
+  puts "renaming #{old_name} to #{new_name}"
+  File.rename old_name, new_name
+end
 
+puts "\n\nall done now -- bye!"
